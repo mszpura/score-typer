1a76ca577e0bd5410e01ffcd1389e5d38759fca9
szpur <maciej.szpura@rydoo.com>
2022-01-31 23:15:30 +0100

    fix abstraction

--- src/app/api/routes/users_route.py
+++ src/app/api/routes/users_route.py
@@ -2,10 +2,10 @@ from typing import List
 from uuid import UUID, uuid4
 
 from fastapi import APIRouter, HTTPException, Depends
-from app.adapters.repositories.users_repo import UsersRepository
 from app.api.composition import compose_user_repository
 from app.api.dtos.user_dto import UserDto
 from app.core.entities.user import User
+from app.core.proxies import AbstractRepository
 
 router = APIRouter()
 
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.post("", response_model=User, status_code=201)
 async def create_user(
         payload: UserDto,
-        repo: UsersRepository = Depends(compose_user_repository)):
+        repo: AbstractRepository = Depends(compose_user_repository)):
     user_id = uuid4()
     user = payload.to_domain(user_id)
     await repo.create(user)
@@ -23,7 +23,7 @@ async def create_user(
 @router.get("/{user_id}", response_model=User)
 async def get_user(
         user_id: UUID,
-        repo: UsersRepository = Depends(compose_user_repository)):
+        repo: AbstractRepository = Depends(compose_user_repository)):
     user = await repo.get(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -31,7 +31,7 @@ async def get_user(
 
 
 @router.get("", response_model=List[User])
-async def get_all(repo: UsersRepository = Depends(compose_user_repository)):
+async def get_all(repo: AbstractRepository = Depends(compose_user_repository)):
     return await repo.get_all()
 
 
@@ -39,7 +39,7 @@ async def get_all(repo: UsersRepository = Depends(compose_user_repository)):
 async def update_user(
         user_id: UUID,
         payload: UserDto,
-        repo: UsersRepository = Depends(compose_user_repository)):
+        repo: AbstractRepository = Depends(compose_user_repository)):
     user = await repo.get(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -52,7 +52,7 @@ async def update_user(
 @router.delete("/{user_id}", response_model=User)
 async def delete_user(
         user_id: UUID,
-        repo: UsersRepository = Depends(compose_user_repository)):
+        repo: AbstractRepository = Depends(compose_user_repository)):
     user = await repo.get(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
